cd597427b3d1cbacccf26ebb95fd65de59da1273
Luanardo <nguyenphucsyluan91@gmail.com>
2023-10-27 11:34:11 +0700

    feat: change student ui

--- app/models/Assignment.js
+++ app/models/Assignment.js
@@ -18,7 +18,7 @@ var assignmentSchema = new Schema({
 		type: Schema.Types.ObjectId,
 		ref: 'Year'
 	},
-	anouncements: [{ type: Schema.Types.ObjectId, ref: 'Anouncement' }],
+	announcements: [{ type: Schema.Types.ObjectId, ref: 'Announcement' }],
 	exercises: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }],
 	schedules: [{ type: Schema.Types.ObjectId, ref: 'Schedule' }],
 	scoreTables: [{ type: Schema.Types.ObjectId, ref: 'ScoreTable' }],
